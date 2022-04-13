a7a62467483f46d5e377b71426e2731c120ed901
Santiago Torres <santiago.torres1234.stg@gmail.com>
2022-04-13 16:36:43 -0500

    God mb role give on payment done

---
+++ events/client/mbFee.js
@@ -0,0 +1,20 @@
+const { ROLES } = require('../../server-config.json')
+
+module.exports = {
+    name: 'mbFee',
+    async execute(message, Discord) {
+        if (!message.content.toLowerCase().startsWith('rpg give ')) return
+        if (!message.content.includes('313351494361677845') || !message.content.includes('707788113677647944')) return
+
+        const filter = (m) => m.author.id === message.author.id;
+        const collector = new Discord.MessageCollector(message.channel, {
+            filter,
+            time: 1000 * 3
+        });
+
+        collector.on('collect', ()=>{
+            if(message.content.includes('300,000,000 coins')) return message.member.roles.add(ROLES.GOD_MB)
+        })
+
+    }
+}
